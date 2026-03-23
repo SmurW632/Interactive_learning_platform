7cb8a5e150e6fd8a1787812539d76779f38eb331
Демин Михаил <smurw.developer@gmail.com>
2026-03-23 14:43:53 +1000

    build(config): обновление файлов конфигурации. Настроена режим запуска. Создан тестовый api для проверки соединения client+server

---
+++ ILP/src/client/ilp.client/src/components/TestConnectionServer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Тест связи с сервером</h1>
+    <button @click="testConnection">Проверить</button>
+    <p v-if="result">{{ result }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const result = ref("");
+
+const testConnection = async () => {
+  try {
+    const response = await fetch("/api/v1/Test/connection");
+    const data = await response.json();
+    result.value = data.message;
+  } catch (error) {
+    result.value = "Ошибка: " + error.message;
+  }
+};
+</script>
--- ILP/src/client/ilp.client/vue.config.js
+++ ILP/src/client/ilp.client/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5005',
+        changeOrigin: true
+      }
+    }
+  },
   transpileDependencies: true
 })
