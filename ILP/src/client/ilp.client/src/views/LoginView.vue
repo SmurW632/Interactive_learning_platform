243ac99775ef4e574f08c69288d5fe9f12854ae7
Kinoe141 <molcanovamargarita321@gmail.com>
2026-03-31 20:27:51 +1000

    feat(ui): создание страниц Home, Login, Profile ILP-17

---
+++ ILP/src/client/ilp.client/src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Вход в систему</h1>
+    <button @click="login">Войти</button>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from '@/stores/user'
+import { useRouter } from 'vue-router'
+
+const userStore = useUserStore()
+const router = useRouter()
+
+const login = () => {
+  userStore.setToken('fake-token-123')
+  router.push('/profile')
+}
+</script>
---
+++ ILP/src/client/ilp.client/src/views/ProfileView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Личный кабинет</h1>
+    <p>Это защищенная страница</p>
+    <button @click="logout">Выйти</button>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from '@/stores/user'
+import { useRouter } from 'vue-router'
+
+const userStore = useUserStore()
+const router = useRouter()
+
+const logout = () => {
+  userStore.logout()
+  router.push('/login')
+}
+</script>
