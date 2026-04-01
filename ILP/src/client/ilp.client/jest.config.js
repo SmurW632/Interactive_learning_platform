e91616767b46fcfce314cb48e58320d90b9204d4
Anticed <Sasha-pankratov-2019@mail.ru>
2026-04-01 19:40:40 +1000

    test(server,client): добавлены изолированные тесты xUnit и Jest

---
+++ ILP/src/client/ilp.client/jest.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleFileExtensions: ['vue', 'js', 'json'],
+  transformIgnorePatterns: ['/node_modules/(?!(vue|@vue/test-utils)/)']
+};
---
+++ ILP/src/server/MyTests/SimpleMathTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace server.Tests;
+
+public class SimpleMathTest
+{
+    [Fact]
+    public void TwoPlusTwoEqualsFour()
+    {
+        int result = 2 + 2;
+        Assert.Equal(4, result);
+    }
+}
