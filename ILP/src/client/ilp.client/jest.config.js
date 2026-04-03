c332264195719960c2a2e20a0b60bcb32be41585
Anticed <Sasha-pankratov-2019@mail.ru>
2026-04-03 20:32:19 +1000

    fix(tests): Испрвавлена структура тестового проекта и обновлена конфигурация

--- ILP/src/client/ilp.client/jest.config.js
+++ ILP/src/client/ilp.client/jest.config.js
@@ -1,9 +1,1 @@
-﻿module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.vue$': '@vue/vue3-jest',
-    '^.+\\.js$': 'babel-jest'
-  },
-  moduleFileExtensions: ['vue', 'js', 'json'],
-  transformIgnorePatterns: ['/node_modules/(?!(vue|@vue/test-utils)/)']
-};
+﻿
