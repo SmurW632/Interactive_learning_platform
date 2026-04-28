cdc5213af50f69a9122dd331fa2a91015a799a62
Демин Михаил <smurw.developer@gmail.com>
2026-04-28 15:57:48 +1000

    refactor(ci-cd): добавлен докер файл для сервера, обновлена конфигурация ci-cd

--- ILP/src/client/ilp.client/vue.config.js
+++ ILP/src/client/ilp.client/vue.config.js
@@ -1,13 +1,30 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Interactive_learning_platform/'
+    : '/',
+  outputDir: 'dist',
+  assetsDir: 'assets',
+  indexPath: 'index.html',
   devServer: {
-    port: 3000,
+    port: 54114,
     proxy: {
       '/api': {
-        target: 'http://localhost:5005',
-        changeOrigin: true
+        target: process.env.VUE_APP_API_URL || 'http://localhost:5004',
+        changeOrigin: true,
+      },
+      '/swagger': {
+        target: process.env.VUE_APP_API_URL || 'http://localhost:5004',
+        changeOrigin: true,
+      },
+      '/research': {
+        target: process.env.VUE_APP_API_URL || 'http://localhost:8001',
+        changeOrigin: true,
       }
+    },
+    historyApiFallback: {
+      index: '/hub.html'
     }
-  },
-  transpileDependencies: true
+  }
 })
