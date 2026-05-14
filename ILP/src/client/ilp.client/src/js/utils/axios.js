04b009bc386829a55837546ca36ad7f1ca12d0a3
Демин Михаил <smurw.developer@gmail.com>
2026-05-14 20:50:35 +1000

    fix(front): исправление url + log

--- ILP/src/client/ilp.client/src/js/utils/axios.js
+++ ILP/src/client/ilp.client/src/js/utils/axios.js
@@ -4,12 +4,19 @@ import axios from 'axios'
 //const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:5004'
 
 const isProduction = window.location.hostname !== 'localhost'
+const baseURL = isProduction
+  ? 'https://ilp-backend-re98.onrender.com/api/v1'   // ✅ URL вашего бэкенда
+  : '/api/v1'
+
+console.log('🌐 [axios] isProduction:', isProduction)
+console.log('🌐 [axios] baseURL:', baseURL)
 
 const axiosInstance = axios.create({
-  baseURL: isProduction
-    ? 'https://ilp-backend-re98.onrender.com/api/v1'
-    : '/api/v1',  // для локальной разработки используем прокси
+  baseURL: baseURL,  // для локальной разработки используем прокси
+  timeout: 30000
 })
+
+
 
 // const axiosInstance = axios.create({
 //   baseURL: `${API_URL}/api/v1`,
